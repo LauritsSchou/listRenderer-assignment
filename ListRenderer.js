9c81f2c8025a94fecbc0ae9735cb4f01c80950fd
LauritsSchou <laurits.schou@gmail.com>
2023-09-29 12:43:40 +0200

    MemberRenderer working as intended

--- ListRenderer.js
+++ ListRenderer.js
@@ -1,6 +1,8 @@
 function construct(list, container, itemRenderer) {
   const ListRenderer = {
+    container: document.querySelector(container),
     render() {
+      console.log(container);
       for (const item of list) {
         const html = itemRenderer.render(item);
         this.container.insertAdjacentHTML("beforeend", html);
